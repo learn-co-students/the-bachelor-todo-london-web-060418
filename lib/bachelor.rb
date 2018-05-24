7ad333179e1f678aa98aff626b364d8bb91fc6ef
ioreka <lucymhodges@gmail.com>
2018-05-24 12:47:52 +0100

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,57 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner_first_name = nil
+  data[season].each do |galdem|
+    if galdem["status"] == "Winner"
+      winner_first_name = galdem.fetch("name").split[0].to_s
+    end
+  end
+  return winner_first_name
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  contestant_name = nil
+  data.each do |season, galdem_data|
+    galdem_data.each do |contestant|
+      if contestant["occupation"] == occupation
+        contestant_name = contestant["name"]
+    end
+  end
+end
+  return contestant_name
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |season, galdem_data|
+    galdem_data.each do |contestant|
+      if contestant["hometown"] == hometown
+        counter += 1
+      end
+    end
+  end
+  return counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, galdem_data|
+    galdem_data.each do |contestant|
+      if contestant["hometown"] == hometown
+        return contestant["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age_counter_sum = 0
+  number_of_ladies = 0
+  age_counter_average = 0
+  data[season].each do |galdem|
+    age_counter_sum += galdem["age"].to_f
+    number_of_ladies += 1
+  end
+  age_counter_average = age_counter_sum / number_of_ladies
+  return age_counter_average.round
 end
